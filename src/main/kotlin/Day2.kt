199bf0fd0ce2f9a30c28670ed939a489516904b4
theboxmage <theboxmage@gmail.com>
2023-12-02 00:35:47 -0500

    Initial Commit

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -2,11 +2,11 @@ import java.io.File
 import java.lang.Integer.max
 
 fun main() {
-    val text = File("Data/D2P1.txt").useLines{it.map{i->i.replace("[,;]".toRegex(), "")}.map{i -> i.split(" ")}.toList()}
+    val text = File("Data/D2P1.txt").useLines{it.map{i->i.replace("[,;:]".toRegex(), "")}.map{i -> i.split(" ")}.toList()}
     var part1 = 0L
     var part2 = 0L
     text.forEach {
-        val id = it[1].substring(0, it[1].length-1).toInt()
+        val id = it[1].substring(0, it[1].length).toInt()
         val map = mutableMapOf<String, Int>()
         for(i in 2 until it.size step 2) {
             map.put(it[i+1], max(it[i].toInt(), map.getOrDefault(it[i+1], 0)))
